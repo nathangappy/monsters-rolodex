643d12f57a3eeaa07d8b6b753419b849538a58ad
g4pm0n3y <nate.gappy@gmail.com>
2019-07-22 16:32:23 -0400

    add page styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 
 import CardList from './components/card-list/card-list';
+import SearchBox from './components/search-box/search-box';
 
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
       monsters: [],
       searchField: ''
-    }
+    };
+
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value});
   }
 
   render() {
@@ -28,11 +33,10 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <input 
-          type='search' 
-          placeholder='search monster' 
-          onChange={e => this.setState({ searchField: e.target.value })}
-        />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+          placeholder='search monsters'
+          handleChange={this.handleChange} />
         <CardList monsters={filteredMonsters} />
       </div>
     )
